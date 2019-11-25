820044436c51a68f1c8e1ccd499209e6c3201ab1
Kenneth Chi <44655752+Kennethchi@users.noreply.github.com>
2019-11-25 04:22:05 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ preprocess_input_mode = "tf"  #"tf" "torch" "caffe"
 #model = keras.applications.inception_resnet_v2.InceptionResNetV2(weights = "imagenet") # tf
 #model = keras.applications.resnet50.ResNet50(weights = "imagenet") # caffe
 #model = keras.applications.xception.Xception(weights = "imagenet") # tf
-model = keras.applications.nasnet.NASNetMobile(weights = "imagenet")
+model = keras.applications.mobilenet.MobileNet(weights = "imagenet")
 
 
 #model.save("InceptionResNetV2.h5")
